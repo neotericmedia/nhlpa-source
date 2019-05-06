6df80c39654c279e4c621b05450f550194638914
Garrigan <DGarrigan@nhlpa.com>
2019-05-06 12:47:49 -0400

    news theme created, reshuffle news component

--- ClientApp/app/components/shared/news-feed/news-feed.component.ts
+++ ClientApp/app/components/shared/news-feed/news-feed.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-news-feed',
   templateUrl: './news-feed.component.html',
-  styleUrls: ['./news-feed.component.scss']
+  styleUrls: ['./news-feed.component.scss'],
+  encapsulation: ViewEncapsulation.Emulated 
 })
 export class NewsFeedComponent implements OnInit {
 
