9caebc284c95d8510cdc467983b4c3d017b3fa08
Garrigan <DGarrigan@nhlpa.com>
2019-05-06 13:24:46 -0400

    scaled out extended featured

--- ClientApp/app/components/extended-feature/extended-feature.component.ts
+++ ClientApp/app/components/extended-feature/extended-feature.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-extended-feature',
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./extended-feature.component.scss']
 })
 export class ExtendedFeatureComponent implements OnInit {
+
+  @Input() news:string;
 
   constructor() { }
 
